f0a7c19427252bf61f765bb9718c0be36ee2b773
adriangrahl <adrian.grahl@gmail.com>
2020-05-22 08:06:15 -0300

    Reference implementation for clients using authorization code grant type with PKCE

--- client.js
+++ client.js
@@ -9,6 +9,36 @@ const config = {
 
 let accessToken = "";
 
+function generateCodeVerifier() {
+  let codeVerifier = generateRandomString(128);
+  localStorage.setItem("codeVerifier", codeVerifier);
+
+  return codeVerifier;
+}
+
+function generateRandomString(length) {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  
+  return text;
+}
+
+function generateCodeChallenge(codeVerifier) {
+  return base64URL(CryptoJS.SHA256(codeVerifier));
+}
+
+function getCodeVerifier() {
+  return localStorage.getItem("codeVerifier");
+}
+
+function base64URL(string) {
+  return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
 function consultar() {
   alert("Consultando recurso com access token " + accessToken);
 
@@ -33,25 +63,21 @@ function consultar() {
 
 function gerarAccessToken(code) {
   alert("Gerando access token com code " + code);
+ 
+  let codeVerifier = getCodeVerifier();
 
-  let clientAuth = btoa(config.clientId + ":" + config.clientSecret);
-  
   let params = new URLSearchParams();
   params.append("grant_type", "authorization_code");
   params.append("code", code);
   params.append("redirect_uri", config.callbackUrl);
+  params.append("client_id", config.clientId);
+  params.append("code_verifier", codeVerifier);
 
   $.ajax({
     url: config.tokenUrl,
     type: "post",
     data: params.toString(),
     contentType: "application/x-www-form-urlencoded",
-
-    beforeSend: function(request) {
-      request.setRequestHeader("Authorization", "Basic " + clientAuth);
-	  console.log('vai requisitar access token');
-	  console.log(request);
-    },
 
     success: function(response) {
       accessToken = response.access_token;
@@ -66,34 +92,20 @@ function gerarAccessToken(code) {
 }
 
 function login() {
-  // https://auth0.com/docs/protocols/oauth2/oauth-state
-  let state = btoa(Math.random());
-  localStorage.setItem("clientState", state);
+  let codeVerifier = generateCodeVerifier();
+  let codeChallenge = generateCodeChallenge(codeVerifier);
 
-  console.log('vai encaminhar para url de autorização do authorization server - authorization code grant type');
-  let urlAuthorizationServer = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&state=${state}&redirect_uri=${config.callbackUrl}`;
-  
-  alert(urlAuthorizationServer);
-  
-  window.location.href = urlAuthorizationServer;
+  window.location.href = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&redirect_uri=${config.callbackUrl}&code_challenge_method=s256&code_challenge=${codeChallenge}`;
 }
 
 $(document).ready(function() {
   let params = new URLSearchParams(window.location.search);
 
   let code = params.get("code");
-  let state = params.get("state");
-  let currentState = localStorage.getItem("clientState");
 
   if (code) {
-	  alert("redirecionado com sucesso!")
     // window.history.replaceState(null, null, "/");
-
-    if (currentState == state) {
-      gerarAccessToken(code);
-    } else {
-      alert("State inválido");
-    }
+    gerarAccessToken(code);
   }
 });
 
